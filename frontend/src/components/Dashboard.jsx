c52f289d0feb0a636b508d829e406f72f4ac22b4
jonathanvra <jonathan.vra@gmail.com>
2024-10-07 14:48:03 -0500

    feat: Creating the Drawer in Dashboard.jsx

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,7 +1,25 @@
-
+import {
+  Box,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Toolbar,
+} from '@mui/material';
 
 export default function Dashboard() {
   return (
-    <div>dashboard</div>
-  )
+    <Drawer anchor='left' open={true} variant='persistent'>
+      <Toolbar>
+        <Box>Busqueda de productos</Box>
+      </Toolbar>
+      <Divider />
+      <List>
+        <ListItem button>
+          <ListItemText primary='Configuración' />
+        </ListItem>
+      </List>
+    </Drawer>
+  );
 }
